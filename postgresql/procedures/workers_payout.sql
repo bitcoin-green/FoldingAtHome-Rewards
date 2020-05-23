2c38827ffa65fe05df8821de6c2cb928ec8e03bf
Nadro-J <n4dro@protonmail.com>
2020-05-23 13:56:36 +0000

    update SQL query

--- postgresql/procedures/workers_payout.sql
+++ postgresql/procedures/workers_payout.sql
@@ -1,22 +1,24 @@
 /*
-CREATE's three temp TABLE's
+CREATE three temp TABLE's
     + tmp_recent_snapshot
         (Holds the most recent snapshot taken of all workers)
 
     + tmp_previous_snapshot
         (Holds the 2nd last snapshot of all workers)
 
-    + tmp_completed_work - [Holds all workers that have contributed]
-        (Calculated using inner join with recent & previous;
-         R.credits - P.credits
-         R.worker_wus - P.worker_wus)
+    + tmp_completed_work - holds all workers that have contributed.
+        Calculated using inner join with recent & previous;
+        R.credits - P.credits
+        R.worker_wus - P.worker_wus
 
-SELECT all workers that don't have 0 credit_diff and 0 wus_diff.
+    SELECT all workers that have completed work units since the last pay-out
 
-drop temp tables at the end
-    * tmp_recent_snapshot
-    * tmp_completed_work
-    * tmp_completed_work
+    DROP all temp tables created.
+    [
+    'tmp_recent_snapshot',
+    'tmp_completed_work',
+    'tmp_completed_work'
+    ]
  */
 
 DROP TABLE IF EXISTS tmp_recent_snapshot;
@@ -24,23 +26,27 @@ DROP TABLE IF EXISTS tmp_previous_snapshot;
 DROP TABLE IF EXISTS tmp_completed_work;
 
 CREATE TEMP TABLE tmp_recent_snapshot AS
-SELECT DISTINCT ON (worker_wus) *
-FROM fath_workers recent
-WHERE recent.lastupdate =
-        (SELECT MAX(lastupdate)
-         FROM fath_workers)
-ORDER BY recent.worker_wus DESC;
+SELECT worker.*
+from fath_workers worker
+where worker.lastupdate = (
+    SELECT DISTINCT ON (recent.lastupdate) recent.lastupdate
+    FROM fath_workers recent
+    ORDER BY recent.lastupdate DESC
+    LIMIT 1
+    )
+ORDER BY worker.name DESC;
 
 CREATE TEMP TABLE tmp_previous_snapshot AS
-SELECT DISTINCT ON (worker_wus) *
-FROM fath_workers previous
-WHERE previous.lastupdate =
-        (SELECT MAX(lastupdate)
-         FROM fath_workers
-         WHERE lastupdate <
-                 (SELECT MAX(lastupdate)
-                  FROM fath_workers) )
-ORDER BY previous.worker_wus DESC;
+SELECT worker.*
+from fath_workers worker
+where worker.lastupdate = (
+    SELECT DISTINCT ON (previous.lastupdate) previous.lastupdate
+    FROM fath_workers previous
+    ORDER BY previous.lastupdate DESC
+    OFFSET 1
+    LIMIT 1
+    )
+ORDER BY worker.name DESC;
 
 CREATE TEMP TABLE tmp_completed_work AS
 SELECT R.wus,
